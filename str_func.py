ab80c3ea22e2916ceeaa34a4bd291c253ba095f2
vlad <borison7890@gmail.com>
2024-04-02 22:19:32 +0200

    add switch_from_lower_to_title and dockstrings to both funcs

--- str_func.py
+++ str_func.py
@@ -1,2 +1,11 @@
 def switch_from_lower_to_upper(str):
+    '''
+    Функция, принимающая str, выводящая STR
+    '''
     return str.upper()
+
+def switch_from_lower_to_title(str):
+    '''
+    Функция, принимающая строку, и выводящая в title регистре
+    '''
+    return str.title()
